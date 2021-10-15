83970daecd995e7477b4d9d37319b159ecb5579f
RafaelDev_ <rafaellima61app@gmail.com>
2021-10-15 19:03:11 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+let hh = 0;
+let mm = 0;
+let ss = 0;
+
+let time = 1000;
+let cron;
+
+function start() {
+  cron = setInterval(() => {
+    timer();
+  }, time);
+}
+
+function pause() {
+    clearInterval(cron)
+}
+
+function stop() {
+    clearInterval(cron);
+     hh = 0;
+     mm = 0;
+     ss = 0;
+
+     document.getElementById("counter").innerText = '00:00:00';
+}
+
+function timer() {
+
+    ss++;
+
+    if(ss == 60) {
+        ss = 0;
+        mm++;
+
+        if(mm == 60) {
+            mm = 0;
+            hh++;
+        }
+    }
+
+    let format = (hh < 10 ? '0' + hh : hh) + ':' + (mm < 10 ? '0' + mm : mm) + ':' + (ss < 10 ? '0' + ss : ss)
+
+  document.getElementById("counter").innerText = format;
+}
